e9570cd5ba79c2d14fc1702d9b244c31290dcb1a
Kartik Nagre <kartik@Kartiks-MacBook-Air.local>
2016-10-24 12:51:31 -0400

    Changed directory path to save pubmed open access data

--- nsf_data_ingestion/pubmed_open_access/download.py
+++ nsf_data_ingestion/pubmed_open_access/download.py
@@ -1,10 +1,10 @@
 # Script to download the Data from Pubmed Open Access
 import urllib
-#urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.A-B.xml.tar.gz", filename="/users/kanagre/comm_use.A-B.xml.tar.gz")
-#urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.C-H.xml.tar.gz", filename="/users/kanagre/comm_use.C-H.xml.tar.gz")
-#urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.I-N.xml.tar.gz", filename="/users/kanagre/comm_use.I-N.xml.tar.gz")
-#urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.O-Z.xml.tar.gz", filename="/users/kanagre/comm_use.O-Z.xml.tar.gz")
+urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.A-B.xml.tar.gz", filename="/users/kanagre/pubmed_data/comm_use.A-B.xml.tar.gz")
+urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.C-H.xml.tar.gz", filename="/users/kanagre/pubmed_data/comm_use.C-H.xml.tar.gz")
+urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.I-N.xml.tar.gz", filename="/users/kanagre/pubmed_data/comm_use.I-N.xml.tar.gz")
+urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.O-Z.xml.tar.gz", filename="/users/kanagre/pubmed_data/comm_use.O-Z.xml.tar.gz")
 urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.A-B.xml.tar.gz", filename="/users/kanagre/pubmed_data/non_comm_use.A-B.xml.tar.gz")
-#urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.C-H.xml.tar.gz", filename="/users/kanagre/non_comm_use.C-H.xml.tar.gz")
-#urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.I-N.xml.tar.gz", filename="/users/kanagre/non_comm_use.I-N.xml.tar.gz")
-#urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.O-Z.xml.tar.gz", filename="/users/kanagre/non_comm_use.O-Z.xml.tar.gz")
+urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.C-H.xml.tar.gz", filename="/users/kanagre/pubmed_data/non_comm_use.C-H.xml.tar.gz")
+urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.I-N.xml.tar.gz", filename="/users/kanagre/pubmed_data/non_comm_use.I-N.xml.tar.gz")
+urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.O-Z.xml.tar.gz", filename="/users/kanagre/pubmed_data/non_comm_use.O-Z.xml.tar.gz")
