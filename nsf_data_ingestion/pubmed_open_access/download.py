9e871f0fcfaa196c1c0b050f70387e7466632242
Kartik Nagre <kartik@Kartiks-MacBook-Air.local>
2016-11-09 13:06:20 -0500

    Added command line arguements

--- nsf_data_ingestion/pubmed_open_access/download.py
+++ nsf_data_ingestion/pubmed_open_access/download.py
@@ -5,6 +5,7 @@ import tarfile,sys
 from shutil import copyfile
 from shutil import rmtree
 import yaml
+import sys
 
 #urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.A-B.xml.tar.gz", filename="/users/kanagre/pubmed_data/comm_use.A-B.xml.tar.gz")
 #urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/comm_use.C-H.xml.tar.gz", filename="/users/kanagre/pubmed_data/comm_use.C-H.xml.tar.gz")
@@ -22,8 +23,12 @@ def chunk_data():
     with open('location.yaml', 'r') as file:
         location = yaml.load(file)
 
-    directory_path_data = location["path"]["input_data"]
-    directory_path_processed = location["path"]["chunk_data"]
+    #directory_path_data = location["path"]["input_data"]
+    directory_path_data = sys.argv[1:][0]
+    #directory_path_processed = location["path"]["chunk_data"]
+    directory_path_processed = sys.argv[1:][1]
+    chunk_size = int(sys.argv[1:][2])
+
     rmtree(directory_path_processed)
     os.makedirs(directory_path_processed)
 
@@ -35,7 +40,7 @@ def chunk_data():
     for subdir, dirs, files in os.walk(directory_path_data):
         for file in files:
             if file.endswith('.nxml'):
-                if count%2000 == 0:
+                if count%chunk_size == 0:
                     ncount  += 1
                     count += 1
                     new_directory_path = directory_path_processed + '/' + str(ncount)
