86c201c868346c2cbb7d85085f977c5503a3f06c
Daniel E. Acuna <daniel.ernesto.acuna@gmail.com>
2017-03-02 17:11:34 -0500

    Create folder

--- nsf_data_ingestion/pubmedoa_hdfs/download_pubmedoa.sh
+++ nsf_data_ingestion/pubmedoa_hdfs/download_pubmedoa.sh
@@ -3,6 +3,8 @@
 # download
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/*.xml.tar.gz
 
+mkdir pubmedoa
+
 # untar to pubmedoa folder
 tar -xzf comm_use.A-B.xml.tar.gz --directory pubmedoa/
 python pubmedoa_hdfs.py
