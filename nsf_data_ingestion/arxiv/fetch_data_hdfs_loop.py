40e45089aef6b1060e61dd0e21c7c8fb0f5da3ec
Daniel Acuna <deacuna@syr.edu>
2017-06-05 14:53:07 -0400

    Some comments and formatting

--- nsf_data_ingestion/arxiv/fetch_data_hdfs_loop.py
+++ nsf_data_ingestion/arxiv/fetch_data_hdfs_loop.py
@@ -1,95 +1,117 @@
 import urllib.request
 import time
 from subprocess import call
-import sys 
+import sys
 import os
 
-retry_time = 45
-retry_n = 3
 
-def get_raw_data():
-	start = time.clock()
-	query = "http://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=oai_dc"
-	print("request: %s" % (query))
-	request = urllib.request.Request(query)
-	response = urllib.request.urlopen(request).read().decode('utf-8')
-	req_error(response)
-	rawfile = open('papers_0.xml','w')
-	rawfile.write(response)
-	rawfile.close()
+def get_raw_data(save_path, retry_time=45, retry_attempts=3):
+    """
+    Retrieve (and resume) XML files from Arxiv and save to HDFS path
 
-	save_to_hdfs("papers_0.xml")
+    :param save_path: HDFS Path
+    :param retry_time: Time interval between two requests
+    :param retry_attempts: If error, retry this many times
+    """
+    start = time.clock()
+    query = "http://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=oai_dc"
+    print("request: %s" % (query))
+    request = urllib.request.Request(query)
+    response = urllib.request.urlopen(request).read().decode('utf-8')
+    req_error(response)
+    rawfile = open('papers_0.xml', 'w')
+    rawfile.write(response)
+    rawfile.close()
 
-	end = time.clock()
-	print("takes: %f s" % (end-start))
-	
-	pos_start = response.rfind('<resumptionToken')
-	pos_end = response.rfind('</resumptionToken')
-	if pos_end > 0 and pos_end > pos_start:
-		pos = response.rfind('>', pos_start, pos_end)
-		resume_token = response[pos+1:pos_end]
-		print("request_resume: %s" % (resume_token))
-		resume = get_resume(resume_token, 1)
-	while resume != "stop":
-		resume = get_resume(resume, 1)
+    save_to_hdfs("papers_0.xml")
 
-def get_resume(token, retry):
-	if retry > retry_n:
-		return "stop"
-	time.sleep(retry_time)
-	start = time.clock()
-	filen = 'papers_%s.xml'%(token.replace('|','_'))
-	try:
-		query = "http://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=%s" % (token)
-		request = urllib.request.Request(query)
-		response = urllib.request.urlopen(request).read().decode('utf-8')
-		req_error(response)
-		rawfile = open(filen,'w')
-		rawfile.write(response)
-		rawfile.close()
-		save_to_hdfs(filen)
+    end = time.clock()
+    print("takes: %f s" % (end - start))
 
-		end = time.clock()
-		print("takes: %f s" % (end-start))
+    pos_start = response.rfind('<resumptionToken')
+    pos_end = response.rfind('</resumptionToken')
+    if pos_end > 0 and pos_end > pos_start:
+        pos = response.rfind('>', pos_start, pos_end)
+        resume_token = response[pos + 1:pos_end]
+        print("request_resume: %s" % (resume_token))
+        resume = get_resume(resume_token, 1, save_path, retry_time, retry_attempts)
+    while resume != "stop":
+        resume = get_resume(resume, 1, save_path, retry_time, retry_attempts)
 
-		pos_start = response.rfind('<resumptionToken')
-		pos_end = response.rfind('</resumptionToken')
-		if pos_end > 0 and pos_end > pos_start:
-			pos = response.rfind('>', pos_start, pos_end)
-			resume_token = response[pos+1:pos_end]
-			print("request_resume: %s" % (resume_token))
-			return resume_token
-		else:
-			return "stop"
-			#get_resume(resume_token)
-	except Exception as err:
-		print(err)
-		print("retry resume_token: %s" % (token))
-		time.sleep(30)
-		retry = retry + 1
-		get_resume(token, retry)
 
-def save_to_hdfs(filename):
-	file = os.path.join(save_path, filename)
-	try:
-		out = call("hdfs dfs -test -e %s" % (file), shell=True)
-		if out == 0:
-			call("hdfs dfs -rm %s" % (file), shell=True)
-			print("file %s exists, delete old one" % (file))
-		call(['hdfs','dfs','-put', filename, save_path])
-		print("send p%s to hdfs" % (filename))
-	except Exception as e:
-		print("save %s to hdfs failed" % (filename))
+def get_resume(token, retry, save_path, retry_time, retry_attempts):
+    """
+    Resume retrieval from a given token
+    :param token: Resume token
+    :param retry: Current attempt
+    :param save_path: HDFS path
+    :param retry_time: Time between requests
+    :param retry_attempts: If error, retry this many times
+    :return: Return resume token
+    """
+    if retry > retry_attempts:
+        return "stop"
+    time.sleep(retry_time)
+    start = time.clock()
+    filen = 'papers_%s.xml' % (token.replace('|', '_'))
+    try:
+        query = "http://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=%s" % (token)
+        request = urllib.request.Request(query)
+        response = urllib.request.urlopen(request).read().decode('utf-8')
+        req_error(response)
+        rawfile = open(filen, 'w')
+        rawfile.write(response)
+        rawfile.close()
+        save_to_hdfs(filen)
+
+        end = time.clock()
+        print("takes: %f s" % (end - start))
+
+        pos_start = response.rfind('<resumptionToken')
+        pos_end = response.rfind('</resumptionToken')
+        if pos_end > 0 and pos_end > pos_start:
+            pos = response.rfind('>', pos_start, pos_end)
+            resume_token = response[pos + 1:pos_end]
+            print("request_resume: %s" % (resume_token))
+            return resume_token
+        else:
+            return "stop"
+
+    except Exception as err:
+        print(err)
+        print("retry resume_token: %s" % (token))
+        time.sleep(30)
+        retry = retry + 1
+        get_resume(token, retry)
+
+
+def save_to_hdfs(filename, save_path):
+    """
+    Save list of XMLs into HDFS
+    :param filename: XML file name
+    :param save_path: HDFS path
+    """
+    file = os.path.join(save_path, filename)
+    try:
+        out = call("hdfs dfs -test -e %s" % (file), shell=True)
+        if out == 0:
+            call("hdfs dfs -rm %s" % (file), shell=True)
+            print("file %s exists, delete old one" % (file))
+        call(['hdfs', 'dfs', '-put', filename, save_path])
+        print("send p%s to hdfs" % (filename))
+    except Exception as e:
+        print("save %s to hdfs failed" % (filename))
+
 
 def req_error(response):
-	start = response.rfind('<error')
-	end = response.rfind('</error>')
-	if start > 0 and end > start:
-		file = open("error.xml", 'w')
-		file.write(response)
-		print(response[start:end])
-		os._exit(0)
+    start = response.rfind('<error')
+    end = response.rfind('</error>')
+    if start > 0 and end > start:
+        file = open("error.xml", 'w')
+        file.write(response)
+        print(response[start:end])
+        os._exit(0)
+
 
 if __name__ == '__main__':
-	save_path = sys.argv[1]
-	get_raw_data()
+    get_raw_data(save_path=sys.argv[1])
--- nsf_data_ingestion/arxiv/process_arxiv.py
+++ nsf_data_ingestion/arxiv/process_arxiv.py
@@ -2,42 +2,51 @@ import xml.etree.ElementTree as ET
 import findspark
 import os
 import sys
-findspark.init('/opt/spark-2.1.0-bin-cdh5.9.1/')
+findspark.init()
 from pyspark.sql import SparkSession
 
+
 def process(file):
-	prefix = "{http://www.openarchives.org/OAI/2.0/}"
-	prefix2 = "{http://purl.org/dc/elements/1.1/}"
+    """
+    Process an Arvix XML file and generates a dictionary with its contents
 
-	root = ET.fromstring(file)
-	records = root.find(prefix + "ListRecords")
-	data = []
-	for record in records.findall(prefix + "record"):
-		identifier = record[0][0].text
-		datastamp = record[0][1].text
-		subjects = []
-		for subject in record[0].findall(prefix + "setSpec"):
-			subjects.append(subject.text)
-		if len(subjects) == 0:
-			subjects.append("none")
+    :param file: XML file
+    :return: list of dictionaries with contents
+    """
+    prefix = "{http://www.openarchives.org/OAI/2.0/}"
+    prefix2 = "{http://purl.org/dc/elements/1.1/}"
 
-		dc = record[1][0]
-		title = dc[0].text
-		authors = []
-		for author in dc.findall(prefix2 + "creator"):
-			authors.append(author.text)
-		abstract = dc.find(prefix2 + "description").text
+    root = ET.fromstring(file)
+    records = root.find(prefix + "ListRecords")
+    data = []
+    for record in records.findall(prefix + "record"):
+        identifier = record[0][0].text
+        datastamp = record[0][1].text
+        subjects = []
+        for subject in record[0].findall(prefix + "setSpec"):
+            subjects.append(subject.text)
+        if len(subjects) == 0:
+            subjects.append("none")
 
-		d = {"id":identifier, "datastamp":datastamp, "title":title, "subjects":subjects, "authors":authors, "abstract":abstract}
-		data.append(d)
-	return data
+        dc = record[1][0]
+        title = dc[0].text
+        authors = []
+        for author in dc.findall(prefix2 + "creator"):
+            authors.append(author.text)
+        abstract = dc.find(prefix2 + "description").text
+
+        d = {"id": identifier, "datastamp": datastamp, "title": title, "subjects": subjects, "authors": authors,
+             "abstract": abstract}
+        data.append(d)
+    return data
+
 
 if __name__ == '__main__':
-  xmlpath = sys.argv[1]
-  parpath = sys.argv[2]
-	spark = SparkSession.builder.getOrCreate()
-	sc = spark.sparkContext
-  filep = os.path.join(xmlpath, "papers_*.xml")
-	xmlfiles = sc.binaryFiles(filep)
-	xmldf = xmlfiles.flatMap(process).toDF()
-	xmldf.write.parquet(parpath)
+    xmlpath = sys.argv[1]
+    parpath = sys.argv[2]
+    spark = SparkSession.builder.getOrCreate()
+    sc = spark.sparkContext
+    filep = os.path.join(xmlpath, "papers_*.xml")
+    xmlfiles = sc.binaryFiles(filep)
+    xmldf = xmlfiles.flatMap(process).toDF()
+    xmldf.write.parquet(parpath)
