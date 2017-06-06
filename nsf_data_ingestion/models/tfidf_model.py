19d8e1476f03272baece7e277a2c267f982af338
Daniel Acuna <deacuna@syr.edu>
2017-06-05 21:33:19 -0400

    Combine data from pubmed, arxiv, and federal exporter

--- nsf_data_ingestion/models/tfidf_model.py
+++ nsf_data_ingestion/models/tfidf_model.py
@@ -30,19 +30,128 @@ def create_spark_session(name):
 
 
 def read_medline(spark, processed_path):
-    """Creates a dataframe with the columns `source` == 'medline', `id` == `pmid`, and
-    `content` == concatenation of abstract, affiliation, author, and journal
+    """Creates a dataframe with the columns:
+    `id`: global id
+    `source`: medline
+    `source_id`: PMID
+    `type`: publication
+    `title`
+    `venue`: journal
+    `abstract`
+    `scientists`: authors
+    `organizations`: affiliation
+    `date`: publication date
+    `content`: concatenation of abstract, affiliation, author, and journal
     """
     medline_path = os.path.join(processed_path, 'medline_last.parquet')
     medline_df = spark.read.parquet(medline_path)
-    return medline_df.select(fn.lit('medline').alias('source'),
-                             fn.col('pmid').astype('int').alias('id'),
-                             fn.concat_ws(' ',
-                                          fn.col('abstract'),
-                                          fn.col('affiliation'),
-                                          fn.col('author'),
-                                          fn.col('journal')).alias('content')
-                             )
+    return medline_df.select(
+            fn.concat(fn.lit('medline_'), fn.col('pmid')).alias('id'),
+            fn.lit('medline').alias('source'),
+            fn.col('pmid').astype('string').alias('source_id'),
+            fn.lit('publication').alias('type'),
+            'title',
+            fn.col('journal').alias('venue'),
+            'abstract',
+            fn.col('author').alias('scientists'),
+            fn.col('affiliation').alias('organizations'),
+            fn.col('pubdate').alias('date'),
+            fn.concat_ws(' ',
+                      fn.col('abstract'),
+                      fn.col('affiliation'),
+                      fn.col('author'),
+                      fn.col('journal')).alias('content'),
+            fn.lit(None).astype('string').alias('end_date'),
+            fn.lit(None).astype('string').alias('city'),
+            fn.lit(None).astype('string').alias('country'),
+            fn.lit(None).astype('string').alias('other_id')
+    )
+
+def read_arxiv(spark, processed_path):
+    """Creates a dataframe with the columns:
+    `id`: global id
+    `source`: arxiv
+    `source_id`: arxiv id
+    `type`: publication
+    `title`
+    `venue`: concatenation of subjects
+    `abstract`
+    `scientists`: authors
+    `organizations`: null
+    `date`: publication date
+    `content`: concatenation of abstract, affiliation, author, and journal
+    """
+    arxiv_path = os.path.join(processed_path, 'arxiv.parquet')
+    arxiv_df = spark.read.parquet(arxiv_path)
+    return arxiv_df.select(
+        fn.concat(fn.lit('arxiv_'), fn.col('id')).alias('id'),
+        fn.lit('arxiv').alias('source'),
+        fn.col('id').astype('string').alias('source_id'),
+        fn.lit('publication').alias('type'),
+        'title',
+        fn.concat_ws('; ', 'subjects').alias('venue'),
+        'abstract',
+        fn.concat_ws(';', 'authors').alias('scientists'),
+        fn.lit(None).astype('string').alias('organizations'),
+        fn.col('datastamp').alias('date'),
+        fn.concat_ws(' ',
+                  fn.col('abstract'),
+                  fn.col('title'),
+                  fn.concat_ws(' ', 'authors'),
+                  fn.concat_ws(' ', 'subjects')).alias('content'),
+        fn.lit(None).astype('string').alias('end_date'),
+        fn.lit(None).astype('string').alias('city'),
+        fn.lit(None).astype('string').alias('country'),
+        fn.lit(None).astype('string').alias('other_id')
+    )
+
+
+def read_federal_exporter(spark, processed_path):
+    """Creates a dataframe with the columns:
+    `id`: global id
+    `source`: federal_exporter
+    `source_id`: project id
+    `type`: grant
+    `title`: PROJECT_TITLE
+    `venue`: AGENCY
+    `abstract`
+    `scientists`: CONTACT_PI + OTHER_PIS
+    `organizations`: ORGANIZATION_NAME
+    `date`: BUDGET_START_DATE
+    `content`: concatenation of abstract, title, PIs, agency, and organization name
+    `end_date`: BUDGET_END_DATE
+    `city`: ORGANIZATION_CITY
+    `country`: ORGANIZATION_COUNTRY
+    `other_id`: PROJECT_NUMBER
+    """
+    abstracts_df = spark.read.parquet(os.path.join(processed_path, 'abstracts.parquet'))
+    projects_df = spark.read.parquet(os.path.join(processed_path, 'projects.parquet'))
+    together_df = projects_df.join(abstracts_df, 'PROJECT_ID')
+    return together_df.select(fn.concat(fn.lit('fe_'), fn.col('PROJECT_ID')).alias('id'),
+        fn.lit('federal_exporter').alias('source'),
+        fn.col('PROJECT_ID').astype('string').alias('source_id'),
+        fn.lit('grant').alias('type'),
+        fn.col('PROJECT_TITLE').alias('title'),
+        fn.col('AGENCY').alias('venue'),
+        fn.col('ABSTRACT').alias('abstract'),
+        fn.concat_ws('; ',
+                     fn.col('CONTACT_PI_PROJECT_LEADER'),
+                     fn.col('OTHER_PIS')).alias('scientists'),
+        fn.col('ORGANIZATION_NAME').astype('string').alias('organizations'),
+        fn.col('BUDGET_START_DATE').alias('date'),
+        fn.concat_ws(' ',
+                  fn.col('ABSTRACT'),
+                  fn.col('PROJECT_TITLE'),
+                  fn.concat_ws(' ', fn.col('CONTACT_PI_PROJECT_LEADER'), fn.col('OTHER_PIS')),
+                  fn.col('AGENCY'),
+                  fn.col('ORGANIZATION_NAME')
+            ).alias('content'),
+        fn.col('BUDGET_END_DATE').alias('end_date'),
+        fn.col('ORGANIZATION_CITY').alias('city'),
+        fn.col('ORGANIZATION_COUNTRY').alias('country'),
+        fn.col('PROJECT_NUMBER').alias('other_id')
+)
+
 
 def add_rowid(x):
     """Called on a RDD when zipWithIndex() is used"""
@@ -90,24 +199,20 @@ if __name__ == "__main__":
 
     spark = create_spark_session('tfidf-computation')
 
-    # read source
-    medline_df = read_medline(spark, processed_path)
-    # in the future, other sources will be added
-    # ...
-    dataframe_list = [medline_df, ]
-    # put all content together
-    content_df = reduce(DataFrame.unionAll, dataframe_list)
+    # read sources
+    medline_df = read_medline(spark, os.path.join(data_path, 'processed'))
+    arxiv_df = read_arxiv(spark, os.path.join(data_path, 'processed', 'arxiv'))
+    fe_df = read_federal_exporter(spark, os.path.join(data_path, 'raw', 'federal_exporter'))
 
-    tfidf_transformer = fit_tfidf_pipeline(content_df)
+    all_data_df = content_df = reduce(DataFrame.unionAll, dataframe_list)
+
+    tfidf_transformer = fit_tfidf_pipeline(all_data_df)
 
     tfidf_model_path = os.path.join(models_path, 'tfidf_transformer.model')
     tfidf_transformer.write().overwrite().save(tfidf_model_path)
-    # add row number
-    content_windex_rdd = content_df.rdd.zipWithIndex()
-    content_windex_df = content_windex_rdd.map(add_rowid).toDF()
 
-    tfidf_df = tfidf_transformer.transform(content_windex_df). \
-        select('row_id', 'source', 'id', 'tfidf')
+    tfidf_df = tfidf_transformer.transform(all_data_df). \
+        select(all_data_df.columns + ['tfidf'])
 
     tfidf_df.write.parquet(tfidf_path, mode='overwrite')
     spark.stop()
